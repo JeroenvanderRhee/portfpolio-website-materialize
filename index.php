2fb7e480478cde9e5d7f959b41a317a554cdec98
JeroenvanderRhee <32259558+JeroenvanderRhee@users.noreply.github.com>
2018-05-16 10:46:46 +0200

    1.0.1
    Particles toegevoegd

---
+++ index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="css/materialize.min.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+	<title>Portfolio website | Jeroen van der Rhee</title>
+</head>
+
+<body>
+  <nav>
+    <div class="nav-wrapper grey">
+      <a href="#" class="brand-logo">
+      	<img src="img/Logo.png" alt="Logo Jeroen van der Rhee" class="logo-edit"/>
+      </a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down nav-edit">
+        <li><a href="sass.html">About Me</a></li>
+        <li><a href="badges.html">Projects</a></li>
+        <li><a href="collapsible.html">Contact</a></li>
+      </ul>
+    </div>
+  </nav>
+  <header class="edit-header">
+    <h1 class="white-text center">Jeroen van der Rhee</h1>
+    <h3 class="white-text center">Innovative webdeveloper</h3>
+  </header>
+  <!-- particles.js container -->
+   <div id="particles-js">
+     
+   </div>
+
+   <div class="row">
+      <div class="col s4 edit-collum-features">
+          <i class="fas fa-users edit-icons-features"></i>
+          <h4 class="edit-features-h4">User experience</h4>
+          <p class="edit-features-p">
+            The challenge in every project is to give the users a experience they never forget. In consultation with the customer, we choose a CMS system that fits by the project.
+          </p>
+          <p class="edit-btn-features-p">
+            <a class="btn-large ">
+            Read more...
+            </a>
+          </p><!-- 
+          <a class="waves-effect waves-light btn-large edit-btn-readmore right"><i class="fas fa-book"></i>Read more...</a> -->
+      </div>
+      <div class="col s4 edit-collum-features">
+        <!-- Promo Content 2 goes here -->
+        hoi
+      </div>
+      <div class="col s4 edit-collum-features">
+        <!-- Promo Content 3 goes here -->
+        hoi
+      </div>
+    </div>
+  
+
+   <footer class="page-footer grey">
+      <div class="container">
+       <div class="row">
+        <div class="col l6 s12">
+          <h5 class="white-text">Disclaimer:</h5>
+           <p class="grey-text text-lighten-4">©<?=date('Y')?> Jeroen van der Rhee | This website was proudly created by www.jeroenvanderrhee.nl for Bookyourcar. If you have questions or complaints about mine projects, please contact me.</p>
+        </div>
+        <div class="col l4 offset-l2 s12">
+         <h5 class="white-text">Links:</h5>
+           <ul>
+             <li><a class="grey-text text-lighten-3" href="about_us.php">About us</a></li>
+             <li><a class="grey-text text-lighten-3" href="projects.php">Projects</a></li>
+             <li><a class="grey-text text-lighten-3" href="contact.php">Contact</a></li>
+           </ul>
+        </div>
+       </div>
+      </div>
+    </footer>
+</body>
+<script src="js/materialize.min.js"></script>
+<script src="js/particles.js"></script>
+<script src="js/app.js"></script>
+
+</html>
